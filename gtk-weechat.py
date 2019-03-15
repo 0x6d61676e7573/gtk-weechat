e1234134b2631c438816728d6ad6642158b9e760
Magnus Berg <magnusb831@gmail.com>
2019-03-15 18:00:17 +0100

    Added GPL headers.

--- gtk-weechat.py
+++ gtk-weechat.py
@@ -1,3 +1,22 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2011-2019 Sébastien Helleu <flashcode@flashtux.org>
+#
+# This file is part of QWeeChat, a Qt remote GUI for WeeChat.
+#
+# QWeeChat is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# QWeeChat is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with QWeeChat.  If not, see <http://www.gnu.org/licenses/>.
+#
 import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
@@ -13,6 +32,7 @@ class MainWindow(Gtk.Window):
     """but does not matter now."""
     def __init__(self):
         Gtk.Window.__init__(self, title="Gtk-weechat")
+        self.set_default_size(800,600)
         self.connect("destroy", Gtk.main_quit)
         self.set_decorated(False)
         
