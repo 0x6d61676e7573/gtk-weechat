6fb527b0111ce73b2992e68761908b888b25e493
Magnus Berg <magnusb831@gmail.com>
2019-05-18 18:00:28 +0200

    Changed default option for time format, so that seconds are not displayed by default.

--- config.py
+++ config.py
@@ -38,7 +38,7 @@ CONFIG_DEFAULT_OPTIONS = (('relay.server', ''),
                           ('relay.lines', str(CONFIG_DEFAULT_RELAY_LINES)),
                           ('look.debug', 'off'),
                           ('look.statusbar', 'off'),
-                          ('look.buffer_time_format', '%H:%M:%S'))
+                          ('look.buffer_time_format', '%H:%M'))
 
 # Default colors for WeeChat color options (option name, #rgb value)
 CONFIG_DEFAULT_COLOR_OPTIONS = (
